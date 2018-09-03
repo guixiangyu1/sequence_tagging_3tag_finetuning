902e47de8fdd8837b873e4445d7bf692e2ee7376
guixiangyu <guixiangyu1@gmail.com>
2018-09-03 14:22:00 +0800

    inti

--- generate_tag_pred_file.py
+++ generate_tag_pred_file.py
@@ -1,19 +1,22 @@
 
 with open("results/To_be_classified.txt","w") as f:
-    g = open("data/text.txt")
-    h = open("results/tag_pred.txt")
+    while(1):
+        g = open("data/text.txt")
+        h = open("results/tag_pred.txt")
 
-    raw_text_line = g.readline()
-    tag = h.readline()
-    while(tag=='\n'):
+        raw_text_line = g.readline()
         tag = h.readline()
+        while(tag=='\n'):
+            tag = h.readline()
 
 
-    if raw_text_line=="\n" or raw_text_line.startswith("-DOCSTRAT-"):
-        f.write(raw_text_line)
-    else:
-        raw_text_line = raw_text_line.strip()
-        f.write("{} {}".format(raw_text_line, tag))
+        if raw_text_line=="\n" or raw_text_line.startswith("-DOCSTRAT-"):
+            f.write(raw_text_line)
+        if raw_text_line=='':
+            break
+        else:
+            raw_text_line = raw_text_line.strip()
+            f.write("{} {}".format(raw_text_line, tag))
     g.close()
     h.close()
 
