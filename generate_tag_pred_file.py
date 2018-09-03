d403585c8489d7d69005e24dec97fb0bb2df2a1a
guixiangyu <guixiangyu1@gmail.com>
2018-09-03 15:07:41 +0800

    inti

--- generate_tag_pred_file.py
+++ generate_tag_pred_file.py
@@ -10,11 +10,11 @@ with open("results/To_be_classified.txt","w") as f:
 
             if raw_text_line=="\n" or raw_text_line.startswith("-DOCSTRAT-"):
                 f.write(raw_text_line)
-            elif raw_text_line=='':
-                break
             elif len(raw_text_line.strip())!=0:
                 raw_text_line = raw_text_line.strip()
                 f.write("{} {}".format(raw_text_line, tag))
+
+
         g.close()
         h.close()
 
