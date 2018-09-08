668fed0440b2596eb58e638d598a5af25aebf508
guixiangyu <guixiangyu1@gmail.com>
2018-09-09 00:43:47 +0800

    metrix=r

--- train_fine_tuning.py
+++ train_fine_tuning.py
@@ -11,7 +11,7 @@ def main():
     config.dropout          = 0.3
     config.batch_size       = 20
     config.lr_method        = "adam"
-    config.lr               = 0.0005
+    config.lr               = 0.001
     config.lr_decay         = 1.0
     config.clip             = -5.0 # if negative, no clipping
     config.nepoch_no_imprv  = 10
